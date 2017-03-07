df8243f4d1da2672473bcce3117b60aea9abe13e
hannieking <hrrk15@gmail.com>
2017-03-07 16:44:55 +0800

    Add files via upload
    NEW VERSION WITH TOTAL SALES

--- report-sales-dates.php
+++ report-sales-dates.php
@@ -18,12 +18,36 @@ $message = NULL;
 									GROUP BY o.orderID";
 
 	}
-	function getOrderDetails($orderDate) {
-		return $query =" SELECT *
-							       FROM orders o
-						         WHERE o.orderDate = '{$orderDate}'
-		              ";
+
+	function getOrderDetails($orderID) {
+		return $query =" SELECT od.gasID, od.quantity
+							FROM orders o JOIN orderdetails od on o.orderID = od.orderID
+							 WHERE od.orderID = '{$orderID}'
+						GROUP BY od.gasID";
 	}
+
+	function getOrdersFromDate($date) {
+		return $query = "SELECT o.orderID, cus.name, o.orderDate
+						   FROM orders o JOIN orderDetails od ON o.orderID = od.orderID
+								 		 JOIN deliveryDetails dd ON od.orderDetailsID = dd.orderDetailsID
+										 JOIN customers cus ON cus.customerID = o.customerID
+									    WHERE o.orderDate = '{$date}'
+									GROUP BY o.orderID";
+	}
+
+	function getPrice($gasID) {
+		return $query = " SELECT gpa.price
+							FROM gaspricingaudit gpa JOIN gasType gt ON gpa.gasID = gt.gasID
+						   WHERE gpa.gasID = '{$gasID}'
+					    ORDER BY gpa.auditID DESC
+					       LIMIT 1";
+	}
+
+	if(isset($_GET['orderDate'])){
+		$orderDate = $_GET['orderDate'];
+		$_SESSION['orderDate'] = $orderDate;
+	}
+
 
 	if(isset($_POST['show-report'])){
 
@@ -112,7 +136,8 @@ $message = NULL;
 								<?php
 									date_default_timezone_set('Asia/Manila');
 									$timestamp = date("F j, Y // g:i a");
-									echo '<b>' .$timestamp. '</b>';
+									echo "Date: ";
+									echo $timestamp;
 								?>
 							</h7>
 						</div>
@@ -121,6 +146,14 @@ $message = NULL;
 					<div class="row">
 						<h3>
 					</div>
+
+				  <div class="pull-left">
+				  <h2>DATE</h2>
+				  </div>
+				  <div class="pull-right">
+				  <h2 class="text-right">TOTAL SALES</h2>
+				  </div>
+				  <div class="clearfix"></div>
 
 					<div class="row">
 						<table class="table table-bordered table-striped">
@@ -131,17 +164,36 @@ $message = NULL;
 			                    				AND o.orderDate <= '{$_SESSION['enddate']}'
 			                                 group by o.orderDate";
 
+
 			                    $result = mysqli_query($dbc,$query);
+
 			                while($row=mysqli_fetch_array($result)){
 											if(!isset($message) && isset($_SESSION['enddate']) && isset($_SESSION['startdate'])){
+												$totalSales = 0;
+												$orderRangeQueryResult = mysqli_query($dbc,getOrdersFromDate($_SESSION['orderDate'])); //GETTING ORDERS FROM SPECIFIED DATE
+												while($orders=mysqli_fetch_array($orderRangeQueryResult,MYSQL_ASSOC) ){ //LOOPING THROUGH ORDERS THAT ARE WITHIN DAT
+													$detailsQueryResult = mysqli_query($dbc,getOrderDetails($orders['orderID']));
+													while($details=mysqli_fetch_array($detailsQueryResult,MYSQL_ASSOC)){ //LOOPING THROUGH GASES FROM ORDER TO GET TOTAL PRICE
+														$sum=0;
+														$priceQueryResult = mysqli_query($dbc,getPrice($details['gasID']));
+														$price = mysqli_fetch_array($priceQueryResult,MYSQL_ASSOC);
+
+														$sum += $details['quantity'] * $price['price']; //
+														$totalSales += $sum;
+
+													}
+												}
+												$totalSales = number_format($totalSales, 2);
+
 													echo "<tr>
-															<td align=\"center\"><a href='report-show-sales.php?orderDate={$row['orderDate']}'>".$row['orderDate']."</td>
+															<td align=\"left\"><a href='report-show-sales.php?orderDate={$row['orderDate']}'>".$row['orderDate']."</td>
+															<td align=\"right\"style=\"font-weight:900\">{$totalSales}</td>
 															</tr>";
 
 											}
 			                }
 			                //$_SESSION['orderDate'] = "<a href='report-show-sales.php?orderDate={$row['orderDate']}'>";
-						?>
+											?>
 						</table>
 						<br>
 						<br>
